bdabdb9cdca0df817e7aa61966ce41a9d205a996
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-05-28 12:08:51 -0400

    created a mappable interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -2,6 +2,14 @@ import { User } from './User';
 import { Company } from './Company';
 // Classes have a dual nature in TS. When you create a class you can create an instance of the class as an object. You can ALSO use a class, to refer to that type as well.
 
+// instructions to every other class on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -19,22 +27,12 @@ export class CustomMap {
     );
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMaker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
